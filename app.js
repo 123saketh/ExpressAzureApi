b102c9e68e0930d124ba9463455e5286b3701541
sakethd <sakethd@zenoti.com>
2021-01-07 12:18:21 +0530

    main

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express  from "express";
+import cors from 'cors';
 
 
 const app=express();
 
-
+app.use(cors());
 
 app.get('/',(req,res)=>{
-    res.send("App running perfectly again!!!");
+    res.send("App running perfectly!!!");
 });
 
-const port=process.env.port||    5000;
+app.get('/:id',(req,res)=>{
+    res.send(req.params.id);
+});
+
+const port=process.env.port || 5000;
 
 app.listen(port,()=>{
     console.log(`Listening on ${port}`);
